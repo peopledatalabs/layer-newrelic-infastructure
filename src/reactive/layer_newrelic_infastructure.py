554c0962b8506239d27368828122a1d0cdac571a
ryan.redman@peopledatalabs.com <ryan.redman@peopledatalabs.com>
2019-04-26 21:01:43 +0000

    I fail at spelling...

--- src/reactive/layer_newrelic_infastructure.py
+++ src/reactive/layer_newrelic_infastructure.py
@@ -1,20 +1,29 @@
-from charms.reactive import when, when_not, set_flag
-from charmhelpers.core.hookenv import status_set
-import charms.apt
+from charms.reactive import (
+    when,
+    when_not,
+    set_flag,
+)
 
 from charmhelpers.core.hookenv import (
     config,
+    status_set,
 )
 
 
-@when_not('layer-newrelic-infastructure.installed')
-def install_layer_newrelic_infastructure():
-    newrelic_infra_yml = open("/etc/newrelic-infra.yml", "w")
-    newrelic_infra_yml.write("license_key: " + config('license_key'))
-    status_set('active', "Installed")
-    set_flag('layer-newrelic-infastructure.installed')
+@when_not('layer-newrelic-infrastructure.installed')
+def install_layer_newrelic_infrastructure():
+    set_flag('layer-newrelic-infrastructure.installed')
 
-# @when('layer-newrelic-infastucture.installed')
-# @when_not('license-key.set')
-# def set_license_key():
-#     set_flag('license-key.set')
+
+@when('config.changed', 'layer-newrelic-infrastructure.installed')
+def set_license_key():
+    status_set('waiting', "Initializing New Relic Infrastructure Agent")
+
+    if config('license_key'):
+        newrelic_infra_yml = open("/etc/newrelic-infra.yml", "w")
+        newrelic_infra_yml.write("license_key: " + config('license_key'))
+
+        status_set('active', "New Relic Infrastructure Agent Ready")
+        set_flag('license-key.set')
+    else:
+        status_set('blocked', "Missing New Relic License Key")
