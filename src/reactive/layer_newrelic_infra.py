bab9fdd06b5b40785eaf4383e2017d7ad3531757
ryan.redman@peopledatalabs.com <ryan.redman@peopledatalabs.com>
2019-05-02 18:15:54 +0000

    Add clean up for newrelic-infra and integrations

--- src/reactive/layer_newrelic_infra.py
+++ src/reactive/layer_newrelic_infra.py
@@ -4,6 +4,7 @@ from charms.reactive import (
     when_not,
     set_flag,
     clear_flag,
+    hook,
 )
 
 from charmhelpers.core.hookenv import (
@@ -17,6 +18,13 @@ from charmhelpers.core.host import (
 
 import charms.apt
 import shutil
+import os
+
+
+@when('newrelic-infra.ready')
+def newrelic_infra_ready():
+    service_restart('newrelic-infra')
+    status_set('active', "New Relic Infrastructure Agent Ready")
 
 
 @when_any('config.changed')
@@ -37,9 +45,14 @@ def configure_agent():
         clear_flag('nri_redis.included')
 
 
+# License Key
 @when('newrelic-infra.license_key.update')
 def set_license_key():
     if not config('license_key'):
+
+        if os.path.isfile("/etc/newrelic-infra.yml"):
+            os.remove("/etc/newrelic-infra.yml")
+
         status_set('blocked', "Missing New Relic License Key")
     else:
         newrelic_infra_yml = open("/etc/newrelic-infra.yml", "w")
@@ -49,11 +62,20 @@ def set_license_key():
         set_flag('newrelic-infra.ready')
 
 
+# New Relic Elasticsearch Integration
 @when('nri_elasticsearch.included')
 @when_not('apt.installed.nri-elasticsearch')
 def install_nri_elasticsearch():
     status_set('waiting', "Installing nri-elasticsearch")
     charms.apt.queue_install(['nri-elasticsearch'])
+
+
+@when_not('nri_elasticsearch.included')
+@when('apt.installed.nri-elasticsearch')
+def remove_nri_elasticsearch():
+    status_set('maintenance', "Removing nri-elasticsearch")
+    charms.apt.purge(['nri-elasticsearch'])
+    set_flag('newrelic-infra.ready')
 
 
 @when('apt.installed.nri-elasticsearch')
@@ -66,11 +88,20 @@ def configure_nri_elasticsearch():
     set_flag('newrelic-infra.ready')
 
 
+# New Relic Redis Integration
 @when('nri_redis.included')
 @when_not('apt.installed.nri-redis')
 def install_nri_redis():
     status_set('waiting', "Installing nri-redis")
     charms.apt.queue_install(['nri-redis'])
+
+
+@when_not('nri_redis.included')
+@when('apt.installed.nri-redis')
+def remove_nri_redis():
+    status_set('maintenance', "Removing nri-redis")
+    charms.apt.purge(['nri-redis'])
+    set_flag('newrelic-infra.ready')
 
 
 @when('apt.installed.nri-redis')
@@ -83,7 +114,20 @@ def configure_nri_redis():
     set_flag('newrelic-infra.ready')
 
 
-@when('newrelic-infra.ready')
-def newrelic_infra_ready():
-    service_restart('newrelic-infra')
-    status_set('active', "New Relic Infrastructure Agent Ready")
+@hook('stop')
+def remove_newrelic_infra():
+    status_set('maintenance', "Removing New Relic Infrastructure Agent")
+
+    if os.path.isfile("/etc/newrelic-infra.yml"):
+        os.remove("/etc/newrelic-infra.yml")
+
+    clear_flag('newrelic-infra.ready')
+    charms.apt.purge(['newrelic-infra'])
+
+    clear_flag('nri_elasticsearch.included')
+    charms.apt.purge(['nri-elasticsearch'])
+
+    clear_flag('nri_redis.included')
+    charms.apt.purge(['nri-redis'])
+
+   
